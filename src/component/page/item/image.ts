a7a1eca38de4568e12a95fe6fc2f369d608dd0ca
juo <hwgiwh271@gmail.com>
2021-08-22 03:34:15 +0900

    feat: dragging left and right
    -

--- src/component/page/item/image.ts
+++ src/component/page/item/image.ts
@@ -2,12 +2,18 @@ import { BaseComponent } from "./../../component.js";
 
 export class ImageComopnent extends BaseComponent<HTMLImageElement> {
   private ImageElement: HTMLImageElement;
+  private img_L: number = 0;
+  private targetElemLeftRight: HTMLElement;
 
   constructor(src: string, title: string = "이미지를 불러오지 못했습니다.") {
     super(`
     <section class="image" >
         <div class="image__holder">
+          <div id="resize">
+            <div class="dragging__leftRight" style="left:0px;">
               <img class="image__thumbnail"/>
+            </div>
+          </div>
         </div>
     </section>
 `);
@@ -19,5 +25,40 @@ export class ImageComopnent extends BaseComponent<HTMLImageElement> {
 
     this.ImageElement.src = src;
     this.ImageElement.alt = title;
+
+    // 이미지 좌우 드래깅
+    this.targetElemLeftRight = this.element.querySelector(
+      ".dragging__leftRight"
+    )! as HTMLElement;
+    this.targetElemLeftRight.addEventListener("mousedown", (e) => {
+      const target = e.currentTarget as HTMLElement;
+      if (target.className !== "dragging__leftRight") return;
+      this.startDragLeftRight(e, this.targetElemLeftRight);
+    });
+  }
+  getLeft(img: HTMLElement) {
+    return parseInt(img.style.left.replace("px", ""));
+  }
+
+  // 이미지를 좌우로
+  moveDragLeftRight = (event: any) => {
+    const dmvx = parseInt(event.clientX + this.img_L);
+
+    this.targetElemLeftRight.style.left = dmvx + "px";
+  };
+
+  // 좌우 드래그
+  startDragLeftRight = (event: any, img: HTMLElement) => {
+    this.targetElemLeftRight = img;
+    this.img_L = this.getLeft(img) - event.clientX;
+    document.onmousemove = this.moveDragLeftRight;
+    document.onmouseup = this.stopDrag;
+    if (event.preventDefault) event.preventDefault();
+  };
+
+  // 드래그를 멈추기
+  stopDrag() {
+    document.onmousemove = null;
+    document.onmouseup = null;
   }
 }

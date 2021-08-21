56a35e8f7ee5df01e7c58a39d5a25d949ff16071
juo <hwgiwh271@gmail.com>
2021-08-22 02:27:37 +0900

    feat: add fileReader in dialog
    1. 이미지 파일 추가 시 이미지 불러오기
    2. 이미지 파일 삭제 시 적용하기 (파일삭제 + 데이터(url) 삭제)
    3. submit 시 App에 이미지 붙이기
    
    -

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { PageComopnent } from "./component/page/page.js";
 import { Dialog } from "./component/page/dialog/dialog.js";
 import { ImageComopnent } from "./component/page/item/image.js";
-
 class App {
   private page: PageComopnent;
   constructor(appRoot: HTMLElement) {
@@ -12,6 +11,17 @@ class App {
     startBtn.addEventListener("click", () => {
       const dialog = new Dialog();
       dialog.attatchTo(appRoot, "beforebegin");
+
+      dialog.setOnSubmit((src: string[], progressBar: HTMLProgressElement) => {
+        let runtime: number = 0;
+        if (!src.length) throw new Error("이미지를 첨부하세요");
+        for (let imgUrl of src) {
+          runtime++;
+          const image = new ImageComopnent(imgUrl);
+          this.page.addChild(image);
+          progressBar.value = (runtime / src.length) * 100;
+        }
+      });
     });
   }
 }
--- src/component/page/dialog/dialog.ts
+++ src/component/page/dialog/dialog.ts
@@ -1,6 +1,11 @@
 import { BaseComponent } from "../../component.js";
 
+type OnSubmit = (imgUrl: string[], progress: HTMLProgressElement) => void;
+
 export class Dialog extends BaseComponent<HTMLFormElement> {
+  private onSubmit?: OnSubmit;
+  private imgUrl: string[] = [];
+
   constructor() {
     super(`
       <form class="dialog no-print">
@@ -38,6 +43,116 @@ export class Dialog extends BaseComponent<HTMLFormElement> {
     )! as HTMLButtonElement;
     cancelBtn.addEventListener("click", () => {
       this.element.remove();
+    });
+
+    const imgFileAddBtn = this.element.querySelector(
+      "#img_file"
+    )! as HTMLInputElement;
+
+    const imgFileDeleteBtn = this.element.querySelector(
+      ".upload-file__delete"
+    )! as HTMLButtonElement;
+
+    const imgCount = this.element.querySelector(
+      ".upload-file__count"
+    )! as HTMLInputElement;
+
+    const imgList = this.element.querySelector(
+      ".upload-file__lists"
+    )! as HTMLUListElement;
+
+    const progressBar = this.element.querySelector(
+      ".progress-bar"
+    )! as HTMLProgressElement;
+
+    // 파일 추가버튼 클릭 후 파일 추가 시
+    imgFileAddBtn.onchange = (e: any) => {
+      const files = [...e.target.files];
+      const readers = [];
+
+      if (!files.length) return;
+
+      for (let i = 0; i < files.length; i++) {
+        readers.push(this.readAsDataURL(files[i]));
+      }
+
+      Promise.all(readers)
+        .then((dataUrls) => {
+          let newDataUrl = [...dataUrls] as string[];
+
+          // 이미지url 저장
+          this.imgUrl = newDataUrl;
+        })
+        .then(() => {
+          imgList.innerHTML = `${files
+            .map(
+              (file: any, id: number) =>
+                `<li class="upload-file__list">
+                    <label for="fileCheck${id}">${file.name}</label>
+                    <input type="checkbox" id="fileCheck${id}" class="checkbox"/>
+                </li>`
+            )
+            .join("")}`;
+          imgCount.textContent = `총 파일 : ${imgList.childElementCount} 개`;
+        });
+    };
+
+    // 파일 체크 후 삭제버튼 클릭 시
+    imgFileDeleteBtn.addEventListener("click", () => {
+      if (!imgList.childElementCount) return;
+
+      const checkedItems = [...imgList.querySelectorAll(".checkbox")].filter(
+        (item) => (item as HTMLInputElement).checked
+      )! as HTMLInputElement[];
+
+      // 체크한 파일의 id들만 받아오기
+      const targetIds = checkedItems.map((item) =>
+        item.getAttribute("id")?.replace(/[^0-9]/g, "")
+      );
+
+      // 체크한 파일 지우기
+      const checkedTargetlists = checkedItems.map(
+        (checked) => checked.parentElement
+      );
+
+      checkedTargetlists.forEach((target) => target?.remove());
+
+      // 체크된 item의 아이디를 내림차순 정렬 후, 해당 url을 삭제
+      targetIds
+        .sort((a, b) => Number(b) - Number(a)) // 뒤에있는거 부터 지우기 위해
+        .forEach((id) => this.imgUrl.splice(Number(id), 1));
+
+      imgCount.textContent = `총 파일 : ${imgList.childElementCount} 개`;
+    });
+
+    // run 클릭 시
+    const dialog = this.element! as HTMLFormElement;
+    dialog.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+      this.onSubmit && this.onSubmit(this.imgUrl, progressBar);
+      setTimeout(() => {
+        this.element.remove();
+      }, 500);
+    });
+  }
+
+  setOnSubmit(listener: OnSubmit) {
+    this.onSubmit = listener;
+  }
+
+  readAsDataURL(file: any) {
+    return new Promise(function (resolve, reject) {
+      const fileReader = new FileReader();
+
+      fileReader.onload = function () {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = function () {
+        reject(fileReader);
+      };
+
+      fileReader.readAsDataURL(file);
     });
   }
 }
